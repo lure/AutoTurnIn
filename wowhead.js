256c0a662cedf6301387ac8001375795a79ee8eb
Alex Shubert <spamme@not.ru>
2012-08-17 23:47:55 +0400

    ... and forget to commit wowhead.js

---
+++ wowhead.js
@@ -0,0 +1,13 @@
+var quests={}, _result="", x;
+var area = $("div.comment-edit-body textarea");
+$("table.listview-mode-default tr").find("td:first div a").each(function (){
+    if ($(this).text() != "") quests[$(this).text()]= "";
+});
+for(x in quests) {
+  _result += "['"+x+"']=\"\",\n"  
+}
+if (area.length) {
+    area.removeAttr("disabled").val(_result);
+} else  {
+    $("body").append('<textarea class="comment-editbox" name="commentbody" cols="40" rows="10">'+_result+'</textarea>');
+}
