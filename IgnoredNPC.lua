56823414c0f9514c37d8c04d350484340de2b4c8
Alex Shubert <shubert@animatron.com>
2017-03-27 19:18:02 +0300

    update fhishing extravaganza

--- IgnoredNPC.lua
+++ IgnoredNPC.lua
@@ -3,5 +3,6 @@ local addonName, privateTable = ...
 privateTable.IGNORED_NPC = {
 	["87391"] = "fate-twister-seress",
 	["88570"] = "Fate-Twister Tiklal",
-	["111243"] = "Archmage Lan'dalock"
+	["111243"] = "Archmage Lan'dalock",
+	["15077"] = "Riggle Bassbait"
 }
