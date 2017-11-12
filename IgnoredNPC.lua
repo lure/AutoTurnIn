38d943a95f9b6f28610d2ac9fa3cfa6c5a88498a
Alex Shubert <shubert@animatron.com>
2017-11-12 04:12:17 +0200

    ignore some npc

--- IgnoredNPC.lua
+++ IgnoredNPC.lua
@@ -4,5 +4,7 @@ privateTable.IGNORED_NPC = {
 	["87391"] = "fate-twister-seress",
 	["88570"] = "Fate-Twister Tiklal",
 	["111243"] = "Archmage Lan'dalock",
-	["15077"] = "Riggle Bassbait"
+	["15077"] = "Riggle Bassbait",
+	["119388"] = "Chieftain Hatuun",
+	["127037"] = "nabiru"
 }
