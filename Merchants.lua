925de2d01a0d593352a70aad122e117299cf5130
Lure <alex.shubert@gmail.com>
2023-07-16 19:37:40 -0400

    chore: Cleanup merchants.lua

--- Merchants.lua
+++ Merchants.lua
@@ -17,10 +17,6 @@ local GetContainerItemLink = C_Container and C_Container.GetContainerItemLink or
 local GetContainerItemInfo = C_Container and C_Container.GetContainerItemInfo or GetContainerItemInfo
 local UseContainerItem = C_Container and C_Container.UseContainerItem or UseContainerItem
 
--- Moves localized repair string to bottom a bit -- why?
---MerchantRepairText:SetPoint("BOTTOMLEFT", 14, 35)
---MerchantRepairAllButton:SetPoint('BOTTOMRIGHT', MerchantFrame, 'BOTTOMLEFT', 140, 33)
-
 local SellButton = CreateFrame("Button", "MerchantFrameSellJunkButton", MerchantFrame, ptable.interface10 and "UIPanelButtonTemplate" or "OptionsButtonTemplate")
 SellButton:SetPoint("TOPRIGHT",  -200, -32)
 SellButton:SetText("Sell junk")
